5ff1a48ac9de93c53219fadeb062dc6a2355aa0f
Hridoy <106481465+Hridoy0016@users.noreply.github.com>
2023-02-16 15:31:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ require("./BotCharacters.js");
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can Change this number //
+  gg = "919394181455";
 }
 
 // -------------------------------------------------------------- //
@@ -47,8 +47,8 @@ global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Atlas MD`;
-global.author = process.env.AUTHOR || "by: Team Atlas";
+global.packname = process.env.PACKNAME || `Spector`;
+global.author = process.env.AUTHOR || "by: Hridoy";
 global.port = process.env.PORT || "8000";
 
 
